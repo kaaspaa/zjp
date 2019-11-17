7432df98ef5ce047c742f8ff8196199e9f2127cd
kaaspaa <kasper12.01@wp.pl>
2019-11-17 20:53:44 +0100

    updated update_item method in item.py file. Changed the way it checks out names

--- gilded_rose/item.py
+++ gilded_rose/item.py
@@ -8,20 +8,20 @@ class Item:
         self.quality = quality
 
     def update_item(self):
-        if self.name != "Sulfuras, Hand of Ragnaros":  # zmieniono ifa z Sulfurasem na sam poczatek
+        if not self.name.contains("Sulfuras"):  # zmieniono ifa z Sulfurasem na sam poczatek
             if self.name == "Aged Brie":
                 increase_quality(self)
                 if self.sell_in <= 0:
                     increase_quality(self)
                 decrease_sell_in(self)
-            elif self.name == "Backstage passes to a TAFKAL80ETC concert":
+            elif self.name.contains("Backstage passes"):
                 if self.sell_in <= 0:
                     self.quality = 0
                 else:
                     increase_quality_in_concert_passes(self)
                 decrease_sell_in(self)  # zmniejszenie sell_in
             else:
-                if self.sell_in < 0:
+                if self.sell_in <= 0:
                     decrease_quality(self)
                 decrease_quality(self)
                 decrease_sell_in(self)
